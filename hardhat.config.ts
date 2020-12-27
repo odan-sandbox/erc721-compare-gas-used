c9bb2c6bd415304670f228800b1be89f57e0d1b4
odan <odan3240@gmail.com>
2020-12-27 20:30:32 +0900

    config を ts ファイルにする

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,20 @@
 // @ts-check
 require("@nomiclabs/hardhat-waffle");
+// declaration merging で hre.ethers を生やしているので反映する
+import "@nomiclabs/hardhat-ethers"
 
-/**
- * 
- * @param {import("@ethersproject/contracts").Contract} contract 
- */
-async function getDeployGasUsed(contract) {
+import {task} from "hardhat/config"
+import {Contract}  from "@ethersproject/contracts"
+import {HardhatRuntimeEnvironment} from "hardhat/types"
+
+async function getDeployGasUsed(hre: HardhatRuntimeEnvironment, contract: Contract) {
   const txReceipt = await hre.ethers.provider.getTransactionReceipt(contract.deployTransaction.hash)
 
   return txReceipt.gasUsed.toString()
 }
 
 
-/**
- * 
- * @param {import("@ethersproject/contracts").Contract} contract 
- */
-async function getMintGasUsed(contract) {
+async function getMintGasUsed(hre: HardhatRuntimeEnvironment, contract: Contract) {
   const accounts = await hre.ethers.getSigners();
 
   const tx = await contract.functions["mint(address,uint256)"](accounts[0].address, 1);
@@ -25,12 +23,7 @@ async function getMintGasUsed(contract) {
   return txReceipt.gasUsed.toString()
 }
 
-
-/**
- * 
- * @param {import("@ethersproject/contracts").Contract} contract 
- */
-async function getTransferFromGasUsed(contract) {
+async function getTransferFromGasUsed(hre: HardhatRuntimeEnvironment, contract: Contract) {
   const accounts = await hre.ethers.getSigners();
 
   await contract.functions["mint(address,uint256)"](accounts[0].address, 10);
@@ -42,7 +35,7 @@ async function getTransferFromGasUsed(contract) {
 }
 
 
-task("measure", "deploy/mint/transferFrom のコストを計測").setAction(async () => {
+task("measure", "deploy/mint/transferFrom のコストを計測").setAction(async (_, hre) => {
   const contractNames = [
     "Basic",
     "CaseBurnable",
@@ -58,9 +51,9 @@ task("measure", "deploy/mint/transferFrom のコストを計測").setAction(asyn
     await instance.deployed();
 
     console.log(contractName)
-    console.log("deploy", await getDeployGasUsed(instance))
-    console.log("mint", await getMintGasUsed(instance))
-    console.log("transferFrom", await getTransferFromGasUsed(instance))
+    console.log("deploy", await getDeployGasUsed(hre, instance))
+    console.log("mint", await getMintGasUsed(hre, instance))
+    console.log("transferFrom", await getTransferFromGasUsed(hre, instance))
 
   }
 })
