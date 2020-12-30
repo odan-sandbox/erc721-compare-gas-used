3c59ca70b87dc2097d0d68f12725f4f4cf8f6480
odan <odan3240@gmail.com>
2020-12-31 03:32:27 +0900

    add typechain

--- test/basic.spec.ts
+++ test/basic.spec.ts
@@ -1,11 +1,12 @@
 import hre from "hardhat";
 import "@nomiclabs/hardhat-ethers";
 import { waffleJest } from "@ethereum-waffle/jest";
+import { Basic__factory as BasicFactory } from "../types/ethers-contracts/factories/Basic__factory";
 expect.extend(waffleJest);
 
 describe("Basic", function () {
   it("poyo", async () => {
-    const Basic = await hre.ethers.getContractFactory("Basic");
+    const Basic = (await hre.ethers.getContractFactory("Basic")) as BasicFactory;
     const basic = await Basic.deploy();
 
     await basic.deployed();
