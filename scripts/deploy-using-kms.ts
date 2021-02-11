f67a60db060b7cad1e3fc6c7d1064eb04e3d3aa3
odan <odan3240@gmail.com>
2021-02-11 13:00:24 +0900

    何度でも deploy-using-kms を使えるように

--- scripts/deploy-using-kms.ts
+++ scripts/deploy-using-kms.ts
@@ -1,7 +1,7 @@
 import { KmsProvider } from "aws-kms-provider";
 import env, { ethers } from "hardhat";
 import { HttpNetworkConfig } from "hardhat/types";
-import { Basic__factory as BasicFactory } from "../types/ethers-contracts/factories/Basic__factory";
+import { Basic } from "../types/ethers-contracts/";
 
 function KmsSigner() {
   const region = "us-east-1";
@@ -21,22 +21,23 @@ async function main() {
     addMinter: https://ropsten.etherscan.io/tx/0xadfdd282f5f37a0bd7d6186162c7789d6328faca7196b283da46af41d8198f85
   */
   const signer = KmsSigner();
-  const minter = "0xa802b07c1B5dd0e0E57911c3fB7911a7BCff6622";
-  const Basic = (await ethers.getContractFactory(
+  const to = "0xa802b07c1B5dd0e0E57911c3fB7911a7BCff6622";
+  const contractAddress = "0x69c25CF26Ce2261740Cf6f3BD48C5e143C1F95Aa";
+  const basic = (await ethers.getContractAt(
     "Basic",
+    contractAddress,
     signer
-  )) as BasicFactory;
-
-  const basic = await Basic.deploy();
-
-  basic.connect(signer);
+  )) as Basic;
 
   await basic.deployed();
 
-  const tx = await basic.addMinter(minter);
+  console.log(basic);
+
+  const tokenId = Math.ceil(Math.random() * 100000);
+  const tx = await basic.functions.mint(to, tokenId);
   console.log(tx);
 
-  console.log(await tx.wait());
+  // console.log(await tx.wait());
 }
 
 main()
